ca2ccac3f06c9a777b51e4c5e7690d7d56bfcacb
Marco Accorinti <accosk8@gmail.com>
2015-09-14 16:47:50 +0200

    payload check - only if it's coming from github repo

--- ci.py
+++ ci.py
@@ -1,11 +1,29 @@
-from flask import Flask
-
+from flask import Flask, request, abort
+from hashlib import sha1
+import hmac
+import os
 
 app = Flask(__name__)
 
 
-def main():
-    print('hello')
+@app.route('/', methods=['POST'])
+def root():
+    if confirm_payload(request):
+        handle_github_request(request.get_json(force=True))
+        pass
+    else:
+        abort(403)
+
+
+def handle_github_request(req):
+    # update from git repository
+    # restart the jovabot service
+    pass
+
+
+def confirm_payload(payload):
+    hashed = hmac.new(os.environ('CI_JOVABOT_SECRET_TOKEN'), payload, sha1)
+    return hmac.compare_digest(hashed.hexdigest(), payload.headers['X-Hub-Signature'])
 
 
 if __name__ == '__main__':
