b8e39549699fb5fdc32e8cbcdd1be9d805ad92c5
Marco Accorinti <accosk8@gmail.com>
2015-09-14 21:07:21 +0200

    added first draft pull from origin develop (for now) restart jovabot service

--- ci.py
+++ ci.py
@@ -2,23 +2,31 @@ from flask import Flask, request, abort
 from hashlib import sha1
 import hmac
 import os
+import git
+import subprocess
 
 app = Flask(__name__)
+
+
+# CONFIGURATION
+GIT_DIR = '/home/acco/dev/jovabot/'
 
 
 @app.route('/', methods=['POST'])
 def root():
     if confirm_payload(request):
         handle_github_request(request.get_json(force=True))
-        pass
     else:
         abort(403)
 
 
 def handle_github_request(req):
     # update from git repository
+    g = git.cmd.Git(GIT_DIR)
+    git_ret = g.pull('origin', 'develop')
     # restart the jovabot service
-    pass
+    service_ret = subprocess.call(['/usr/sbin/service', 'jovabot', 'restart'], shell=False)
+    return git_ret, service_ret
 
 
 def confirm_payload(payload):
