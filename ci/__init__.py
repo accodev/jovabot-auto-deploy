90c1ff9462f050dd655d67a12344e614bce255ce
Marco Accorinti <accosk8@gmail.com>
2016-02-25 14:03:40 +0100

    no message

--- ci/__init__.py
+++ ci/__init__.py
@@ -6,6 +6,8 @@ import git
 import subprocess
 import logging
 import array
+import requests
+import time
 
 app = Flask(__name__)
 logging.basicConfig(filename='ci_jovabot.log', filemode='w', level=logging.DEBUG)
@@ -26,14 +28,21 @@ GIT_DIR = '/home/acco/dev/jovabot/'
 
 @app.route('/', methods=['POST'])
 def root():
+    logging.debug('Received request')
     if confirm_payload(request):
-        handle_github_request(request.get_json(force=True))
+        u, g, s = handle_github_request(request.get_json(force=True))
+        logging.info('Received payload from github => u[{}] g[{}] s[{}]'.format(u, g, s))
     else:
+        logging.debug('Request not valid')
         return 'ko', 403
     return 'ok', 200
 
 
 def handle_github_request(req):
+    # dispatch the channel update to jovabot
+    update_ret = jovabot_channel_update(req)
+    # I know, it sucks
+    time.sleep(1)
     # update from git repository
     g = git.cmd.Git(GIT_DIR)
     git_ret = g.pull('origin', 'master')
@@ -41,7 +50,7 @@ def handle_github_request(req):
     if any_file_changed(req):
         # restart the jovabot service
         service_ret = subprocess.call(['sudo', '/usr/sbin/service', 'jovabot', 'restart'], shell=False)
-    return git_ret, service_ret
+    return update_ret, git_ret, service_ret
 
 
 def confirm_payload(payload):
@@ -52,8 +61,7 @@ def confirm_payload(payload):
 
 def any_file_changed(req):
     changed = 0
-    ref = req.get('ref')
-    if ref and 'master' in ref:  # only updates on master
+    if check_for_master_branch(req):
         hc = req.get('head_commit')
         if hc:
             file_list = hc.get('added') + hc.get('removed') + hc.get('modified')
@@ -70,6 +78,19 @@ def find_file_with_exts(file_list, ext=None):
             if file.endswith(tuple(ext)):
                 c = c + 1
     return c
+
+
+def jovabot_channel_update(req):
+    if check_for_master_branch(req):
+        endpoint = socket.gethostname() + '/' + os.environ['JOVABOT_WEBAPP_NAME'] + '/channel-update'
+        return requests.post('{}/{}'.format(endpoint, os.environ('UPDATE_SECRET'), data=req.get('commits')))
+
+
+def check_for_master_branch(req):
+    ref = req.get('ref')
+    if ref and 'master' in ref:  # only updates on master
+        return ref
+    return None
 
 
 if __name__ == '__main__':
